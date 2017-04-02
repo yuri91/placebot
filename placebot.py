ae5efb3368ae3107087e2febcf6d1fc6b5ae7347
Yuri Iozzelli <y.iozzelli@gmail.com>
2017-04-03 00:40:23 +0200

    sleep a bit while searching

--- placebot.py
+++ placebot.py
@@ -109,9 +109,11 @@ while True:
         canvas = Canvas(username, password)
         try:
             x, y, target_color = drawing.get_random_pixel()
+            actual_color = canvas.get_pixel(x,y)
             while actual_color == target_color:
                 x, y, target_color = drawing.get_random_pixel()
                 actual_color = canvas.get_pixel(x,y)
+                time.sleep(0.1)
             print("wrong color at", x, y, actual_color, "instead of", target_color)
             time_to_sleep = canvas.put_pixel(x,y,target_color)
             final_color = canvas.get_pixel(x,y)
